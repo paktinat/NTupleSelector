0f7c5d29048ef759bd918d97bd4788df32115ebb
paktinat <saeid.paktinat@cern.ch>
2014-02-09 17:28:47 +0100

    a little cleaning

--- test/copy_cfg.py
+++ test/copy_cfg.py
@@ -31,7 +31,7 @@ process.NTupleSelector.HLTPaths = ['HLT_QuadJet80_v*'
 # Tell the process which files to use as the sourdce
 process.source = cms.Source ("PoolSource",
 #          fileNames = cms.untracked.vstring ("file:///dataLOCAL/paktinat/Projects/MT2/MT2_V02-03-02/MT2Analysis/Code/MT2AnalysisCode/MT2Code/V03_09_01_MultiJet_Run2012D_PromptReco_v1_NTupleProducer_53X_data_1939_1_wj9.root")
-           fileNames = cms.untracked.vstring ("file:///dataLOCAL/paktinat/Projects/CMSSW/CMSSW_5_3_7_patch1/src/DiLeptonAnalysis/NTupleSelector/test/MultiJet1Parked_D1SNTupleProducer_53X_data_9_2_nCm.root")
+           fileNames = cms.untracked.vstring ("file:MultiJet1Parked_D1SNTupleProducer_53X_data_9_2_nCm.root")
 #fileNames = cms.untracked.vstring ("rfio:///dpm/particles.ipm.ac.ir/home/cms//store/user/paktinat/MultiJet1Parked/V03-09-01_MultiJet1Parked-Run2012D-part1-10Dec2012-v1-SecondHalf/913e379464056a6b529e5016356a02b3/NTupleProducer_53X_data_3347_3_kHY.root")
 
                              
